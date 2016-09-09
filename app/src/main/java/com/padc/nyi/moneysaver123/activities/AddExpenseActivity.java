ba63d8482e2eacc61eb2d185c6db079b2babb174
NyiNyiLin <nyiynilin.dev@gmail.com>
2016-09-09 21:38:11 +0630

    add 'Add Expense Screen'

---
+++ app/src/main/java/com/padc/nyi/moneysaver123/activities/AddExpenseActivity.java
@@ -0,0 +1,46 @@
+package com.padc.nyi.moneysaver123.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.padc.nyi.moneysaver123.MoneySaverApp;
+import com.padc.nyi.moneysaver123.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AddExpenseActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.spinner_category)
+    Spinner spinnerCategory;
+
+    public static Intent newIntent(){
+        Intent intent = new Intent(MoneySaverApp.getContext(), AddExpenseActivity.class);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+        ButterKnife.bind(this, this);
+
+        setSupportActionBar(toolbar);
+
+        final String []dummyCategory={"aaa", "bbb"};
+        ArrayAdapter adapter = new ArrayAdapter(getBaseContext(), R.layout.support_simple_spinner_dropdown_item, dummyCategory);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinnerCategory.setAdapter(adapter);
+
+    }
+
+}
--- app/src/main/java/com/padc/nyi/moneysaver123/fragments/ExpenseFragment.java
+++ app/src/main/java/com/padc/nyi/moneysaver123/fragments/ExpenseFragment.java
@@ -1,18 +1,31 @@
 package com.padc.nyi.moneysaver123.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.padc.nyi.moneysaver123.R;
+import com.padc.nyi.moneysaver123.activities.AddExpenseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by ZMTH on 9/5/2016.
  */
-public class ExpenseFragment extends Fragment {
+public class ExpenseFragment extends Fragment implements View.OnClickListener{
+    @BindView(R.id.rv_expense_list)
+    RecyclerView rvExpenseList;
+
+    @BindView(R.id.fab_add_expense)
+    FloatingActionButton fabAddExpense;
+
     public ExpenseFragment() {
     }
 
@@ -20,6 +33,16 @@ public class ExpenseFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_expense, container, false);
+        ButterKnife.bind(this, view);
+        fabAddExpense.setOnClickListener(this);
+
         return view;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = AddExpenseActivity.newIntent();
+        startActivity(intent);
     }
 }
