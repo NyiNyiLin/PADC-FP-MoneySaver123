5bb2138813de5440e3cc0aa00703fa011a411fbb
ZuneMoeThetHtar <zune.developer@gmail.com>
2016-09-10 17:50:12 +0630

    Implementation of Income List and Add Income from

--- app/src/main/java/com/padc/nyi/moneysaver123/activities/HomeActivity.java
+++ app/src/main/java/com/padc/nyi/moneysaver123/activities/HomeActivity.java
@@ -1,9 +1,7 @@
 package com.padc.nyi.moneysaver123.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.padc.nyi.moneysaver123.R;
 import com.padc.nyi.moneysaver123.fragments.BillFragment;
@@ -32,7 +29,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +38,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         setSupportActionBar(toolbar);
 
-
+        if(savedInstanceState == null){
+            navigateToExpense();
+        }
 
         //Drawer Layout
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
--- app/src/main/java/com/padc/nyi/moneysaver123/fragments/IncomeFragment.java
+++ app/src/main/java/com/padc/nyi/moneysaver123/fragments/IncomeFragment.java
@@ -1,18 +1,32 @@
 package com.padc.nyi.moneysaver123.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.padc.nyi.moneysaver123.R;
+import com.padc.nyi.moneysaver123.activities.AddIncomeActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by ZMTH on 9/5/2016.
  */
-public class IncomeFragment extends Fragment {
+public class IncomeFragment extends Fragment implements View.OnClickListener{
+
+    @BindView(R.id.rv_income_list)
+    RecyclerView rvExpenseList;
+
+    @BindView(R.id.fab_add_income)
+    FloatingActionButton fabAddExpense;
+
     public IncomeFragment() {
     }
 
@@ -20,6 +34,16 @@ public class IncomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_income, container, false);
+
+        ButterKnife.bind(this, view);
+        fabAddExpense.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = AddIncomeActivity.newIntent();
+        startActivity(intent);
     }
 }
