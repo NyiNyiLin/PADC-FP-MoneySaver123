cab8700f56fe69118a0ffbcf04199434ccba5b1d
NyiNyiLin <nyiynilin.dev@gmail.com>
2016-09-16 23:36:42 +0630

    add date Picker

--- app/src/main/java/com/padc/nyi/moneysaver123/activities/AddExpenseActivity.java
+++ app/src/main/java/com/padc/nyi/moneysaver123/activities/AddExpenseActivity.java
@@ -1,6 +1,5 @@
 package com.padc.nyi.moneysaver123.activities;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -11,18 +10,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.padc.nyi.moneysaver123.MoneySaverApp;
 import com.padc.nyi.moneysaver123.R;
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class AddExpenseActivity extends AppCompatActivity {
+public class AddExpenseActivity extends AppCompatActivity implements  DatePickerDialog.OnDateSetListener{
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
@@ -30,7 +35,9 @@ public class AddExpenseActivity extends AppCompatActivity {
     Spinner spinnerCategory;
 
     @BindView(R.id.txt_date)
-    TextView txtDate;
+    TextView tvDate;
+
+    SimpleDateFormat dateFormatter;
 
     public static Intent newIntent(){
         Intent intent = new Intent(MoneySaverApp.getContext(), AddExpenseActivity.class);
@@ -55,7 +62,7 @@ public class AddExpenseActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         spinnerCategory.setAdapter(adapter);
 
-
+        getCurrentDate();
     }
 
     @Override
@@ -71,7 +78,46 @@ public class AddExpenseActivity extends AppCompatActivity {
 
     @OnClick(R.id.txt_date)
     public void onClickDate(View view){
+        showThirdPartyDatePicker();
+    }
+    private void getCurrentDate(){
+        Calendar now = Calendar.getInstance();
+
+        try {
+            dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+            String dateInString = now.get(Calendar.DAY_OF_MONTH) + "/" + (now.get(Calendar.MONTH) + 1) + "/" + now.get(Calendar.YEAR);
+            Date date = dateFormatter.parse(dateInString);
+            dateFormatter = new SimpleDateFormat("dd/MMM/yyyy");
+            tvDate.setText(tvDate.getText().toString() + dateFormatter.format(date).toString());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
+    private  void showThirdPartyDatePicker(){
+        Calendar now = Calendar.getInstance();
+        DatePickerDialog thirdPartyDatePicker = DatePickerDialog.newInstance(
+                this,
+                now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH)
+        );
 
+        thirdPartyDatePicker.show(this.getFragmentManager(), "ThirdPartyDatePicker");
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        try {
+            dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+            String dateInString = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+            Date date = dateFormatter.parse(dateInString);
+            dateFormatter = new SimpleDateFormat("dd/MMM/yyyy");
+            tvDate.setText(dateFormatter.format(date).toString());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
