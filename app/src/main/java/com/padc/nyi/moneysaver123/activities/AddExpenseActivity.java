081e47ceafe3ed821fe2571836c29910bd40cf42
NyiNyiLin <nyiynilin.dev@gmail.com>
2016-09-16 14:27:45 +0630

    edit add expense activity design and change content to Myanmar

--- app/src/main/java/com/padc/nyi/moneysaver123/activities/AddExpenseActivity.java
+++ app/src/main/java/com/padc/nyi/moneysaver123/activities/AddExpenseActivity.java
@@ -1,20 +1,26 @@
 package com.padc.nyi.moneysaver123.activities;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.padc.nyi.moneysaver123.MoneySaverApp;
 import com.padc.nyi.moneysaver123.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AddExpenseActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
@@ -22,6 +28,9 @@ public class AddExpenseActivity extends AppCompatActivity {
 
     @BindView(R.id.spinner_category)
     Spinner spinnerCategory;
+
+    @BindView(R.id.txt_date)
+    TextView txtDate;
 
     public static Intent newIntent(){
         Intent intent = new Intent(MoneySaverApp.getContext(), AddExpenseActivity.class);
@@ -35,11 +44,33 @@ public class AddExpenseActivity extends AppCompatActivity {
         ButterKnife.bind(this, this);
 
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setTitle("သံုးလိုက္ေသာ ေငြထည့္ရန္");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
-        final String []dummyCategory={"aaa", "bbb"};
+        final String []dummyCategory={"အစားအေသာက္", "အဝတ္အစား", "လမ္းစရိတ္", "ေဖ်ာ္ေျဖေရး", "ကားအသံုးစရိတ္", "အေထြေထြ"};
         ArrayAdapter adapter = new ArrayAdapter(getBaseContext(), R.layout.support_simple_spinner_dropdown_item, dummyCategory);
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         spinnerCategory.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case android.R.id.home:
+                this.finish();
+                break;
+        }
+        return true;
+    }
+
+    @OnClick(R.id.txt_date)
+    public void onClickDate(View view){
 
     }
 
